95e99e207889af39539503de678942592c1333ca
Clarence <cjancheta15@gmail.com>
2022-03-24 13:39:51 +0400

    custom http client

--- http.go
+++ http.go
@@ -10,14 +10,29 @@ var (
 	ErrInvalidRequest = errors.New("request return an non success code")
 )
 
-type Client[T any] struct{}
+type Client[T any] struct {
+	HttpClient *http.Client
+}
 
+// NewClient initialize with custom HTTP Client
+func NewClient[T any](httpClient *http.Client) Client[T] {
+	return Client[T]{
+		HttpClient: httpClient,
+	}
+}
+
+// Get calls the given url and decode the response
+// to the given type of the Client
 func (c *Client[T]) Get(url string) (*T, error) {
+	httpClient := http.DefaultClient
+	if c.HttpClient != nil {
+		httpClient = c.HttpClient
+	}
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
-	resp, err := http.DefaultClient.Do(req)
+	resp, err := httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
